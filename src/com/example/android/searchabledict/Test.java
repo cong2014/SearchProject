4743a8b3ae654ae57da0938563bb0490310dc1fc
cong <cong@domyMobile>
2014-02-23 21:57:44 +0800

    second commit for this file2.

--- src/com/example/android/searchabledict/Test.java
+++ src/com/example/android/searchabledict/Test.java
@@ -1,8 +1,14 @@
 package com.example.android.searchabledict;
 
+import android.animation.*;
 public class Test {
 	int x = 1;
 	int y = 10;
 	
 	int z = x + y;
+	
+	int z2 = z * z;
+	
+	int a1 = 100;
+	int a2 = 200;
 }
